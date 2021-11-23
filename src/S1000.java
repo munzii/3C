56f9664755b6ba21c5ef6588a002bc400088b7f1
munzii <munzii422@gmail.com>
2021-11-23 22:07:16 +0900

    brush up

---
+++ src/S1000.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class S1000 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int sum = a+b;
+		
+		bw.write(String.valueOf(sum));
+		
+		br.close();
+		bw.flush();
+		bw.close();
+
+	}
+
+}
