db13cf025d8192fc5a48103d6d4ec2178f40beeb
munzii <munzii422@gmail.com>
2021-12-16 23:03:12 +0900

    studying

---
+++ src/T2446.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class T2446 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter (new OutputStreamWriter(System.out));
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<i; j++) {
+				bw.write(" ");
+			}
+			
+			for(int k=0; k<(2*n-1)-(2*i); k++) {
+				bw.write("*");
+			}
+		}
+		
+		for(int i=0; i<n+1)
+		
+	}
+
+}
